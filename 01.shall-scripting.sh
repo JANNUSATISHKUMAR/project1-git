1efa09ea92c88e6bcb214fbf1574060d320ca878
Jannu Satish Kumar <jannusatishkumar@gmail.com>
2024-04-21 00:12:28 +0530

    bash scripting file

--- 01.shall-scripting.sh
+++ 01.shall-scripting.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+apt-get autoremove -y
+apt-get clean -y
+
 apt install nmap -y >> /temp/progress.log
 apt remove nmap -y >> /temp/progress.log
 apt update -y >> /temp/progress.log
---
+++ bashscript.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+### This is frist line of the script ##
+#
+#echo "Welcome to bash script"
+#echo
+#
+########################################
+#
+#echo "the uptime of the system is:"
+#uptime
+#echo
+#echo
+########################################
+#echo"Memory Utilization:"
+#free -m
+#echo
+#echo
+########################################
+#echo
+#echo "Disk Utilization:"
+#df -m
+#echo
+#echo
+########################################
+#
+#
