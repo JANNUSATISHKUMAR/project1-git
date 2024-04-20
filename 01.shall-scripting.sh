2ed4a71bafef913a495949d0c7363eee26a171ef
Jannu Satish Kumar <jannusatishkumar@gmail.com>
2024-04-20 19:34:16 +0530

    First-commit

--- 01.shall-scripting.sh
+++ 01.shall-scripting.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt install nmap
-apt remove nmap
-apt update
-apt upgrade
+apt install nmap -y >> /temp/progress.log
+apt remove nmap -y >> /temp/progress.log
+apt update -y >> /temp/progress.log
+apt upgrade -y >> /temp/progress.log
