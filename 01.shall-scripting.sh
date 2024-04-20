22dfd03a55a8b7528f3bb48e29350dc38aae0390
Jannu Satish Kumar <jannusatishkumar@gmail.com>
2024-04-21 01:14:31 +0530

    Files-deleted

--- 01.shall-scripting.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-apt-get autoremove -y
-apt-get clean -y
-
-apt install nmap -y >> /temp/progress.log
-apt remove nmap -y >> /temp/progress.log
-apt update -y >> /temp/progress.log
-apt upgrade -y >> /temp/progress.log
